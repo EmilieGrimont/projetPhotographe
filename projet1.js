c6821d645a3e1fe9bd278979f1d090a546748428
EmilieGrimont <emilie.grimont78@gmail.com>
2021-11-05 14:06:36 +0100

    fenêtre de remerciements

--- projet1.js
+++ projet1.js
@@ -3,5 +3,9 @@ function hide(){
     modal.style.display="none"
 }
 document.getElementById('bouton').addEventListener('click',hide)
-
+function salut() {
+    let salutations= document.getElementById('prenom').value;
+    document.getElementById('Hi').innerText='Bonjour '+ salutations
+}
+document.getElementById('bouton').addEventListener('click',salut)
     
