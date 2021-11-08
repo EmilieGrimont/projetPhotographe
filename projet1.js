814c12656c8bacd924f425cd7df4f1219a410628
EmilieGrimont <emilie.grimont78@gmail.com>
2021-11-08 14:21:54 +0100

    Commentaires

--- projet1.js
+++ projet1.js
@@ -1,17 +1,25 @@
+//Pop In caché
 function hide(){
 
     modal.style.display="none";
 }
 document.getElementById("bouton").addEventListener("click",hide);
 
-
+//Bonjour+prénom 
 function salut() {
     let salutations= document.getElementById("prenom").value;
     document.getElementById("Hi").innerText="Bonjour "+ salutations;
 }
 document.getElementById("bouton").addEventListener("click",salut);
 
-document.getElementById("validate").addEventListener("click",remerciement)
+//Confirmation du mail + remerciements
+function remerciement() {
+    let mercibcp= document.getElementById('email').value;
+    document.getElementById("thank").innerText="Merci "+ document.getElementById("prenom").value +", nous te confirmons que tu seras informé sur ton email : "+ mercibcp;
+}
+document.getElementById("validate").addEventListener("click",remerciement);
+
+//Pop-in caché + Pop-in de confirmation d'adresse mail.
 function pop(){
     merci.style.display="none"
 }
@@ -23,46 +31,51 @@ function app(){
 }
 document.getElementById("validate").addEventListener("click",app);
 
-
-function remerciement() {
-    let mercibcp= document.getElementById('email').value;
-    document.getElementById("thank").innerText="Merci "+ document.getElementById("prenom").value +", nous te confirmons que tu seras informé sur ton email : "+ mercibcp;
-}
-document.getElementById("validate").addEventListener("click",remerciement);
-
+//Caché le contenu de la page quand on est sur les pop-in
 function disp(){
     main.style.display="none"
 }
 document.getElementById("validate").addEventListener("click",disp);
+
 function hide2(){
 
     merci.style.display="none";
 }
 document.getElementById("bouton2").addEventListener("click",hide2);
 
+//reprise du prénom + aurevoir + merci
 function aurevoir() {
     let name= document.getElementById("prenom").value;
     document.getElementById("bye2").innerText="Merci" + name + " et à bientôt !";
 }
 document.getElementById("bouton2").addEventListener("click",aurevoir);
 
+//disparition de la photo
 function photo(){
     Photographe.style.display="none";
 }
 photo();
+
+//apparition de la photo 
 function photo1(){
 
     Photographe.style.display="flex";
 }
 document.getElementById("bouton2").addEventListener("click",photo1);
+
+//apparition de la barre de cookie après le 1er pop in
 function cookiesBar(){
     cookies.style.display="flex";
 }
 document.getElementById("bouton").addEventListener("click", cookiesBar);
+
+//quand on accepte, la barre disparait
 function hideCookies(){
     cookies.style.display="none";
 }
 document.getElementById("acceptButton").addEventListener("click",hideCookies);
+
+//quand on clique sur personnaliser, on ouvre le pop in de paramétrage des cookies
 function setCookies(){
     modalCookies.style.display="flex";
 }
